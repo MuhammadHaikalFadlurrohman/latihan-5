09b4ff1d84a5d7593c9d5db5a5f3dc23b3b39f9e
MuhammadHaikalFadlurrohman <haikalfadlurrohman@gmail.com>
2025-09-15 08:58:39 +0700

    masukan

---
+++ array.js
@@ -0,0 +1,16 @@
+var angka = [1,2,3,4,5];
+angka.forEach(function(e){
+    console.log(e);
+});
+
+angka.forEach(num => console.log(num*2));
+
+var angka2 = angka.map(function(e){
+    return e * 2;
+});
+console.log(angka2);
+
+var angka3 = angka2.map(function(e){
+    return e * 3;
+});
+console.log(angka3);
---
+++ array2.js
@@ -0,0 +1,16 @@
+var angka = [1,2,3,4,5];
+var angka2 = angka.map(function(e){
+    return e * 2;
+});
+console.log(angka2);
+
+// var angka3 = angka2.map(function(e){
+//     return e * 3;
+// });
+// console.log(angka3);
+
+// var nama = ['Muhammad', 'Haikal', 'Fadlur', 'rohman'];
+// var nama2 = nama.map(function(e){
+//     return e + ' adalah mahasiswa';
+// });
+// console.log(nama2);
