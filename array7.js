71610767da24b25e519ae080ebafa652dba58e62
MuhammadHaikalFadlurrohman <haikalfadlurrohman@gmail.com>
2025-09-16 07:59:23 +0700

    upgrade

--- array7.js
+++ array7.js
@@ -17,6 +17,20 @@ let mhs2 ={
 mhs1.makan(5);
 mhs2.makan(10);
 
-     
+
+function Mahasiswa(nama, energi){
+    this.nama = nama;
+    this.energi = energi;
+    this.makan = function(porsi){
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan!`);
+    }
+}
+
+let mhs3 = new Mahasiswa('siti', 15);
+let mhs4 = new Mahasiswa('joko', 25);
+mhs3.makan(5);
+mhs4.makan(10);                 
+
 
 
